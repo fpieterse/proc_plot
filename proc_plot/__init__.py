b9a382c16634d96f7c6833b42d3a6dcded469211
Francois Pieterse <francois185@gmail.com>
2020-04-18 06:16:45 +1000

    importing only what you need

--- proc_plot/__init__.py
+++ proc_plot/__init__.py
@@ -6,7 +6,9 @@ time-indexed dataframe.  Tagnames (columns in dataframe) are analysed to group
 related tags (e.g. SP and PV) to plot on the same subplot.
 '''
 
-from .pp import *
+from .pp import add_grouping_rule, \
+                print_grouping_rules, \
+                set_dataframe, show
 
 __all__ = ['add_grouping_rule',
            'print_grouping_rules',
